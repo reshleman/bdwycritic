4f4c68934440f1a5190517f090c23e8ccf429952
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-18 14:30:07 -0400

    Initial Styling
    **Styling:**
    * Add Bourbon, Neat, and Bitters
    * Refactor navigation bar and `events#show` to use Refills components
    * Style `events#show` with separate sections for event description,
    * details, and reviews
    * Use placeholder images next to reviews (to be replaced w/ more visual
    * display of rating)
    * Basic styling to display items on `events#index` in a grid
    
    **Minor Presentation-Related Refactorings:**
    * Rename "Media Review" to "Critic Review" in user-facing copy
    * Extract helper method to format closing dates on `events#show`
    * Refactor `event_preferences#create` JS to remove duplication
    * Refactor `EventPreferencesHelper` to remove unnecessary `id` fields
    * Extract helper method for rendering a user review score as stars
    
    https://trello.com/c/PmjQfZUc
    
    ![screen shot 2014-08-29 at 10 37 31 am](https://cloud.githubusercontent.com/assets/7756801/4091585/3838e032-2f8a-11e4-9f1c-e5464495ade2.png)
    ![screen shot 2014-08-29 at 10 38 28 am](https://cloud.githubusercontent.com/assets/7756801/4091586/383c8e76-2f8a-11e4-913c-4cb6ee4cae74.png)

--- app/helpers/event_preferences_helper.rb
+++ app/helpers/event_preferences_helper.rb
@@ -1,29 +1,25 @@
 module EventPreferencesHelper
   def positive_preference_link(event)
-    link_to "I want to see this",
+    link_to "[I want to see this]",
       event_preferences_path(event, wants_to_see: true),
-      id: "positive-preference-link",
       method: :post,
       remote: true
   end
 
   def negative_preference_link(event)
-    link_to "Not interested",
+    link_to "[I'm not interested]",
       event_preferences_path(event, wants_to_see: false),
-      id: "negative-preference-link",
       method: :post,
       remote: true
   end
 
   def positive_preference_text
     content_tag :span,
-      "You want to see this",
-      id: "positive-preference-text"
+      "You want to see this."
   end
 
   def negative_preference_text
     content_tag :span,
-      "You're not interested",
-      id: "negative-preference-text"
+      "You're not interested."
   end
 end
