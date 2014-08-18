b66cc69576ca9e15d2b73ac3159cad66490e695f
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-18 16:16:59 -0400

    User can only review an event once
    * Add `reviewed_events` relationship to `User` model
    * Add `reviewed?(event)` helper method to `User` model
    * Only show link to `user_reviews#new` when user has not already
    reviewed the event

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_many :user_reviews
+  has_many :reviewed_events, through: :user_reviews, source: :event
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def reviewed?(event)
+    reviewed_events.include?(event)
+  end
 end
