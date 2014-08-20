6e28c921fa33f7b655364e88fb1ef1c124acfc4f
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-20 13:05:58 -0400

    FIXUP Remove testing code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,7 +8,5 @@ class EventsController < ApplicationController
   def index
     @current_events = Event.current
     @closed_events = Event.closed
-
-    @nyt_events_query = NytEvents::Query.new(filters: 'category: "Theater"', limit: 3)
   end
 end
