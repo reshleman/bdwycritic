1156522c19ed5d6ae5423c72aa4dd4bcb2b55080
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-20 15:45:57 -0400

    FIXUP Minor refactoring

--- app/models/events_importer.rb
+++ app/models/events_importer.rb
@@ -14,31 +14,35 @@ class EventsImporter
   attr_reader :nyt_events
 
   def fetch_nyt_events
-    NytEvents::Query.new(filters: 'category: "Theater", subcategory: "Broadway"', limit: 500).events
+    query = NytEvents::Query.new(
+      filters: 'category: "Theater", subcategory: "Broadway"',
+      limit: 500
+    )
+    query.events
   end
 
-  def import_event(nyt_event)
-    event = Event.find_or_initialize_by(nyt_event_id: nyt_event.event_id)
+  def import_event(source)
+    event = Event.find_or_initialize_by(nyt_event_id: source.event_id)
 
-    if event.new_record? || event.older_than_nyt?(nyt_event.last_modified)
-      update_or_create_from(event, nyt_event)
+    if event.new_record? || event.older_than_nyt?(source.last_modified)
+      update_or_create_from(event, source)
     end
   end
 
-  def update_or_create_from(event, source_event)
-    event.name = remove_surrounding_quotes(source_event.event_name)
-    event.description = remove_surrounding_p_tags(source_event.web_description)
-    event.venue = source_event.venue_name
-    event.closing_date = source_event.recurring_end_date
-    event.nyt_updated_at = source_event.last_modified
+  def update_or_create_from(event, source)
+    event.name = strip_single_quotes(source.event_name)
+    event.description = strip_p_tags(source.web_description)
+    event.venue = source.venue_name
+    event.closing_date = source.recurring_end_date
+    event.nyt_updated_at = source.last_modified
     event.save!
   end
 
-  def remove_surrounding_quotes(string)
-    string.gsub(/\A‘|’\Z/, '')
+  def strip_single_quotes(string)
+    string.gsub(/\A‘|’\Z/, "")
   end
 
-  def remove_surrounding_p_tags(string)
-    string.gsub(/\A<p>|<\/p>\Z/, '')
+  def strip_p_tags(string)
+    string.gsub(/\A<p>|<\/p>\Z/, "")
   end
 end
