10c8c8a904215d104c35541594a54c4d80c42798
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-21 14:51:43 -0400

    Implement sentiment analysis methods via API call

--- app/models/sentiment_analyzer.rb
+++ app/models/sentiment_analyzer.rb
@@ -1,17 +1,30 @@
 class SentimentAnalyzer
+  BASE_URL = "http://text-processing.com/api"
+  ENDPOINT = "/sentiment/"
+  URL = BASE_URL + ENDPOINT
+
   def initialize(text)
-    @text = text
+    @post_params = { body: { text: URI::encode_www_form_component(text) } }
+    @query_result = perform_query
   end
 
   def positive
-    0.1
+    query_result["probability"]["pos"]
   end
 
   def negative
-    0.9
+    query_result["probability"]["neg"]
   end
 
   def neutral
-    0.675
+    query_result["probability"]["neutral"]
+  end
+
+  private
+
+  attr_reader :post_params, :query_result
+
+  def perform_query
+    HTTParty::post(URL, post_params)
   end
 end
