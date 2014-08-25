a573e5b1bef36ac1043dbe207e65fc6070ce55b0
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-25 13:24:58 -0400

    As an admin, I can edit and delete user reviews
    **Admins can edit user reviews**
    * Add edit and update routes for `admin/user_reviews`
    * Create `Admin::UserReviewsController`
    * Extract user reviews form fields into a partial
    * Update `user_reviews#new` view to use partial
    * Add `admin/user_reviews#edit` view
    * Add link to `user_review` partial to display edit link for admin users
    
    **Admins can delete user reviews**
    * Add destroy route for `admin/user_reviews`
    * Add destroy action to `Admin::UserReviewsController`
    * Add delete link to `user_review` partial for admins
    
    https://trello.com/c/3D2jEhlC

---
+++ app/controllers/admin/user_reviews_controller.rb
@@ -0,0 +1,33 @@
+class Admin::UserReviewsController < ApplicationController
+  def edit
+    @user_review = find_user_review
+  end
+
+  def update
+    @user_review = find_user_review
+
+    if @user_review.update(user_review_params)
+      redirect_to @user_review.event
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user_review = find_user_review
+    event = user_review.event
+    user_review.destroy
+
+    redirect_to event
+  end
+
+  private
+
+  def find_user_review
+    UserReview.find(params[:id])
+  end
+
+  def user_review_params
+    params.require(:user_review).permit(:body, :rating)
+  end
+end
