7ecfd63dc4747dd19c22daef728a67ad4cb8a70d
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-27 12:28:35 -0400

    AJAX-ify event preference selection
    * Update event preference link helpers to use `remote: true`
    * Update event preference link helpers with `id` attributess for JS
    * Add helpers for event preference text (with `span` tags for JS)
    * Update `links` partial to use new helpers
    * Update `EventPreferencesController` to render a `.js.erb` view
    * Create `event_preferences/create.js.erb` to swap links and text
    
    https://trello.com/c/uk38y9x4

--- app/controllers/event_preferences_controller.rb
+++ app/controllers/event_preferences_controller.rb
@@ -1,12 +1,8 @@
 class EventPreferencesController < ApplicationController
   def create
-    event = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id])
 
-    if current_user.create_or_update_preference(event, wants_to_see?)
-      redirect_to event
-    else
-      redirect_to event, flash: { error: "Something went wrong." }
-    end
+    current_user.create_or_update_preference(@event, wants_to_see?)
   end
 
   private
--- app/helpers/event_preferences_helper.rb
+++ app/helpers/event_preferences_helper.rb
@@ -2,12 +2,28 @@ module EventPreferencesHelper
   def positive_preference_link(event)
     link_to "I want to see this",
       event_preferences_path(event, wants_to_see: true),
-      method: :post
+      id: "positive-preference-link",
+      method: :post,
+      remote: true
   end
 
   def negative_preference_link(event)
     link_to "Not interested",
       event_preferences_path(event, wants_to_see: false),
-      method: :post
+      id: "negative-preference-link",
+      method: :post,
+      remote: true
+  end
+
+  def positive_preference_text
+    content_tag :span,
+      "You want to see this",
+      id: "positive-preference-text"
+  end
+
+  def negative_preference_text
+    content_tag :span,
+      "You're not interested",
+      id: "negative-preference-text"
   end
 end
