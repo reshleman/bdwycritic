6b255bcdf4cf488cd3718f15398d3d53a284b7b8
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-08-27 14:07:42 -0400

    Fix event review counts
    Becuase we're outer joining across multiple tables, we need to use
    `COUNT(DISTINCT ...)` instead of just `COUNT` for these statistics.

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,8 +20,8 @@ class Event < ActiveRecord::Base
 
   def self.with_review_statistics
     select("events.*").
-    select("COUNT(media_reviews.id) AS num_media_reviews").
-    select("COUNT(user_reviews.id) AS num_user_reviews").
+    select("COUNT(DISTINCT media_reviews.id) AS num_media_reviews").
+    select("COUNT(DISTINCT user_reviews.id) AS num_user_reviews").
     select("AVG(media_reviews.sentiment) AS average_media_review").
     select("AVG(user_reviews.rating) AS average_user_review").
     joins("LEFT JOIN media_reviews ON events.id = media_reviews.event_id").
