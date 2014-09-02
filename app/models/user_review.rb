78860e3471d3f2e380423f780f9dc979589f95fd
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-09-02 17:51:26 -0400

    Disallow 0 values for user reviews

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -8,7 +8,7 @@ class UserReview < ActiveRecord::Base
     presence: true,
     numericality: {
       only_integer: true,
-      greater_than_or_equal_to: 0,
+      greater_than_or_equal_to: 1,
       less_than_or_equal_to: 10
     }
   validates :user,
