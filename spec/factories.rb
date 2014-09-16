1d871f83dc365ae7220c667647e6eaa5e7c4df0e
Robert Eshleman <c.robert.eshleman@gmail.com>
2014-09-15 20:59:22 -0400

    Add feature specs for guest visiting event page
    * Add factories for event, event with user reviews, event with media reviews
    * Add specs for proper display of event information, user reviews, and media reviews

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,63 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "user#{n}@example.com"
+  end
+
+  factory :event do
+    closing_date "1947-05-24"
+    description "Carousel is the second musical by Rodgers and Hammerstein."
+    name "Carousel"
+    venue "Majestic Theatre"
+
+    factory :event_with_media_reviews do
+      ignore { media_review_count 2 }
+
+      after :create do |instance, evaluator|
+        create_list(
+          :media_review,
+          evaluator.media_review_count,
+          event: instance
+        )
+      end
+    end
+
+    factory :event_with_user_reviews do
+      ignore { user_review_count 2 }
+
+      after :create do |instance, evaluator|
+        create_list(
+          :user_review,
+          evaluator.user_review_count,
+          event: instance
+        )
+      end
+    end
+  end
+
+  factory :media_review do
+    author "Ben Brantley"
+    event
+    headline "Praise for a Fantastic Show"
+    sentiment 0.9
+    source "New York Times"
+    url "http://www.nytimes.com"
+  end
+
   factory :user do
+    email
     first_name "Hugh"
     last_name "Jackman"
-    email "hugh@jackman.com"
     password_digest "password"
 
     trait :admin do
       admin true
     end
+  end
+
+  factory :user_review do
+    body "This was a fantastic show!"
+    event
+    rating 9
+    user
   end
 end
